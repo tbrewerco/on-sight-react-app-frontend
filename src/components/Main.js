364ae1cdd7a164223448f73e638bf209ba6f91cb
Travis Brewer <t.brewer.co@gmail.com>
2021-07-06 21:44:57 -0600

    wip: declare state variable, func makes api call, func create new route, useEffect to make initial call

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import RouteIndex from "../pages/RouteIndex";
 import RouteShow from "../pages/RouteShow";
 
 function Main(props) {
+
+    const [ routeState, setRouteState ] = useState(null);
+
+    const URL = "http://localhost:4000/routes"
+
+    //////
+    // get data
+    //////
+
+    const getRoutes = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setRouteState(data);
+    }
+
+    //////
+    // create
+    //////
+
+    const createRoute = async (route) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(route),
+        });
+        // update state
+        getRoutes();
+    }
+
+
+    //////
+    // update
+    //////
+    
+
+
+    //////
+    // delete
+    //////
+    
+    useEffect(() => getRoutes(), []); // clean up effect once on mount/ unmount
+
     return (
         <main>
             <Switch>
