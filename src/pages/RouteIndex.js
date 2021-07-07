14713218d71fa821bbd0d024da5fe838d8d5ff97
Travis Brewer <t.brewer.co@gmail.com>
2021-07-06 22:03:12 -0600

    feat: display routes in RouteIndex.js, working

--- src/pages/RouteIndex.js
+++ src/pages/RouteIndex.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 function RouteIndex(props) {
-    return <h1>Index</h1>
+    // loaded routes function
+    const loaded = () => {
+        return props.routes.map((route) => (
+            <div key={route._id} className="route">
+                <Link to={`/routes/${route._id}`}><h3>{route.name}</h3></Link>
+                <img src={route.image} alt={route.name} />
+                <h4>{route.consensus_grade}</h4>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <span>Loading...</span>;
+    };
+
+    return props.routes ? loaded() : loading();
 }
 
 export default RouteIndex;
