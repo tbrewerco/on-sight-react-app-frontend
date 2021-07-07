6eb9f76b6744e874718769773389ef8c424fc388
Travis Brewer <t.brewer.co@gmail.com>
2021-07-06 19:43:25 -0600

    wip: add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header.js"
 import Main from "./components/Main.js"
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
