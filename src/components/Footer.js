64614b00cd3f6e4b94e856bad6c7966e8d4863f5
Travis Brewer <t.brewer.co@gmail.com>
2021-07-09 09:14:30 -0600

    wip: adjust footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import { StyledFooter } from "../styles";
 export default function Footer(props) {
     return (
         <StyledFooter>
-            <p>Footer &copy; All Rights Reserved {new Date().getFullYear}</p>
+            <p> &copy; All Rights Reserved {new Date().getFullYear}</p>
         </StyledFooter>
     )
 }
