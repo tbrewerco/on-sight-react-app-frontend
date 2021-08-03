7f69c733593dc1380257804dba39cb6b2a28ff82
Travis Brewer <t.brewer.co@gmail.com>
2021-08-02 21:18:10 -0600

    feat: renders gym distance to user location conditionally

--- src/components/GymSearch.js
+++ src/components/GymSearch.js
@@ -9,9 +9,18 @@ export default function GymSearch({ gyms }) {
           <Dropdown.Toggle id="dropdown-button-dark-example1" variant="dark">Select a Gym</Dropdown.Toggle>
           <Dropdown.Menu variant="dark">
             {gyms.map(gym => {
-              return (
-                <Dropdown.Item key={gym.name} href={`/gyms/${gym._id}`}>{gym.name} ({gym.distanceFromUser} mi) </Dropdown.Item>
-              )
+              // conditionally display miles on items
+              function hasDistance() {
+                return <Dropdown.Item key={gym.name} href={`/gyms/${gym._id}`}>{gym.name} ({gym.distanceFromUser} mi) </Dropdown.Item>
+              };
+              function noDistance() {
+                return <Dropdown.Item key={gym.name} href={`/gyms/${gym._id}`}>{gym.name}</Dropdown.Item>
+              };
+
+              if (gym.distanceFromUser) {
+                return hasDistance();
+              }
+              return noDistance();
             })}
           </Dropdown.Menu>
         </Dropdown>
