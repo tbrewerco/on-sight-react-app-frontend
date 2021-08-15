41c2740cdbfc474bf3148152ad277ffc33e85231
Travis Brewer <t.brewer.co@gmail.com>
2021-08-14 20:39:27 -0600

    feat: tick now displays user grade selection converted to Hueco or YDS

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -1,7 +1,19 @@
-export default function TickList({ tick, key }) {
+import huecoGrades from "../utils/BoulderGrades";
+import yosemiteGrades from "../utils/grades";
+
+export default function TickList({ tick, key, route }) {
+
+  if (route.route_type === "Sport") {
+    tick.userGrade = yosemiteGrades[tick.difficulty_grade] || "Unknown";
+  } else if (route.route_type === "Boulder") {
+    tick.userGrade = huecoGrades[tick.difficulty_grade] || "Unknown";
+  } else {
+    tick.userGrade = "N/A";
+  }
+
   return (
     <>
-      <p>Past tick: quality rating: {tick.quality_rating} difficulty: {tick.difficulty_grade} comment: {tick.comment} date added: {tick.createdAt}</p>
+      <p>{tick.createdBy} - difficulty: {tick.userGrade} - quality rating: {tick.quality_rating} - comment: {tick.comment} - date added: {tick.createdAt}</p>
       <br />
     </>
   )
