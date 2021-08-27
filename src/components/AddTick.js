c415d5f9316efc71101549694cb133c396b1b7bc
Travis Brewer <t.brewer.co@gmail.com>
2021-08-26 20:04:54 -0600

    wip: adds info icon, removes awesomefont

--- src/components/AddTick.js
+++ src/components/AddTick.js
@@ -1,9 +1,39 @@
+import { useState, useMemo } from "react";
 import { Dropdown, Form, Button, OverlayTrigger, Image, Tooltip } from "react-bootstrap";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
 import huecoGrades from "../utils/BoulderGrades";
 import yosemiteGrades from "../utils/grades";
+import StarRateRoundedIcon from '@material-ui/icons/StarRateRounded';
+import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
 
 export default function AddTick({ route }) {
+  const [count, setCount] = useState(null)
+  const [rating, setRating] = useState(0);
+  const [starColor, setStarColor] = useState(
+    {
+      unfilled: "#656464",
+      filled: "#ede159"
+    }
+  );
+  // console.log(starColor.filled)
+
+  const onRating = () => {
+
+  }
+
+  // const starRating = useMemo(() => {
+  //   return Array(count)
+  //     .fill(0)
+  //     .map((_, i) => i + 1)
+  //     .map((idx) => (
+  //       <FontAwesomeIcon
+  //         key={idx}
+  //         className="cursorPointer"
+  //         icon="star"
+  //       // onClick={() => onRating(idx)}
+  //       />
+  //     ))
+  // }, [count, rating])
 
   const gradesArray = [];
   function buildGradesArray() {
@@ -33,15 +63,19 @@ export default function AddTick({ route }) {
             {/* Tooltip */}
             <OverlayTrigger
               placement="right"
-              overlay={<Tooltip id="tooltip">You have the option of selecting a grade of up to five grades higher or five grades lower than the current "consensus grade"</Tooltip>}>
+              overlay={<Tooltip id="tooltip">You may select a grade of up to five grades higher or five grades lower than the current "consensus grade"</Tooltip>}>
               <Button
                 variant="light"
                 className="d-inline-flex align-items-center">
-                <Image roundedCircle src="" />
-                <span className="ms-1">info</span>
+                <span className="ms-1">{<InfoRoundedIcon />}</span>
               </Button>
+              {/* <InfoRoundedIcon /> */}
             </OverlayTrigger>
           </Dropdown>
+        </div>
+        <div className="starRatingDiv">
+
+
         </div>
       </Form>
     </>
