8e88e0cfa3dac6ba48dfa585dcfc4a79d8b7a154
Travis Brewer <t.brewer.co@gmail.com>
2021-08-27 18:04:46 -0600

    feat: adds star icon to tick component

--- src/components/Tick.js
+++ src/components/Tick.js
@@ -1,7 +1,18 @@
 import huecoGrades from "../utils/BoulderGrades";
 import yosemiteGrades from "../utils/grades";
+import StarRateRoundedIcon from '@material-ui/icons/StarRateRounded';
 
-export default function TickList({ tick, key, route }) {
+export default function TickList({ tick, key, route, starColor }) {
+
+  const tickStarsArray = [];
+  function buildTickStarsArray() {
+    if (tick.quality_rating) {
+      for (var i = 0; i < tick.quality_rating; i++) {
+        tickStarsArray.push(i);
+      }
+    }
+  }
+  buildTickStarsArray();
 
   if (route.route_type === "Sport") {
     tick.userGrade = yosemiteGrades[tick.difficulty_grade] || "Unknown";
@@ -13,7 +24,7 @@ export default function TickList({ tick, key, route }) {
 
   return (
     <>
-      <p>Username goes here {tick.createdBy} - difficulty: {tick.userGrade} - quality rating: {tick.quality_rating}/5 - comment: "{tick.comment}" - date added: {tick.createdAt.slice(0, 10)}</p>
+      <p>Username goes here {tick.createdBy} - difficulty: {tick.userGrade} - quality rating: {tickStarsArray.map(star => { return <StarRateRoundedIcon style={{ color: starColor.filled }} /> })}comment: "{tick.comment}" - date added: {tick.createdAt.slice(0, 10)}</p>
       <br />
     </>
   )
